1a2766e9afdd913d5513e6230691ead06f13fc06
Marcos Dias <marcosa460@gmail.com>
2024-12-04 22:57:08 -0300

    feat: implement CRUD operations for Manga entity
    - Added CRUD methods to MangaService for creating, reading, updating, and deleting Manga.
    - Created routes in MangaController to expose CRUD operations.

--- src/manga/manga.controller.ts
+++ src/manga/manga.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { MangaService } from './manga.service';
+import { Manga } from './manga.entity';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
-@Controller('manga')
-export class MangaController {}
+@Controller('mangas')
+export class MangaController {
+  constructor(private readonly mangaService: MangaService) {}
+
+  @Post()
+  async create(@Body() createMangaDto: Partial<Manga>): Promise<Manga> {
+    return await this.mangaService.create(createMangaDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Manga[]> {
+    return await this.mangaService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Manga> {
+    return await this.mangaService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateMangaDto: Partial<Manga>,
+  ): Promise<Manga> {
+    return await this.mangaService.update(id, updateMangaDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return await this.mangaService.delete(id);
+  }
+}
--- src/manga/manga.service.ts
+++ src/manga/manga.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Manga } from './manga.entity';
 
 @Injectable()
-export class MangaService {}
+export class MangaService {
+  constructor(
+    @InjectRepository(Manga)
+    private readonly mangaRepository: Repository<Manga>,
+  ) {}
+
+  async create(createMangaDto: Partial<Manga>): Promise<Manga> {
+    const newManga = this.mangaRepository.create(createMangaDto);
+    return await this.mangaRepository.save(newManga);
+  }
+
+  async findAll(): Promise<Manga[]> {
+    return await this.mangaRepository.find();
+  }
+
+  async findOne(id: string): Promise<Manga> {
+    return await this.mangaRepository.findOneBy({ id });
+  }
+
+  async update(id: string, updateMangaDto: Partial<Manga>): Promise<Manga> {
+    await this.mangaRepository.update(id, updateMangaDto);
+    return await this.mangaRepository.findOneBy({ id });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.mangaRepository.delete(id);
+  }
+}
